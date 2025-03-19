ad8cb77713cdcca3772555d4bce62f4469a9fcde
Slavomir04 <146534879+Slavomir04@users.noreply.github.com>
2025-03-19 15:22:51 +0100

    update

--- functions/Filter.py
+++ functions/Filter.py
@@ -1,6 +1,6 @@
 import Reduce
 
-def filterWordsCount(array,mincount=4):
+def getZdaniaPodrzedne(array,mincount=4):
     result = []
     for sentence in array:
         s = Reduce.splitFromChars(sentence,ignore=Reduce.nonWhiteValues())
@@ -8,6 +8,34 @@ def filterWordsCount(array,mincount=4):
             result.append(s)
     return result
 
+def getAskYellSentence(array:list[str])->list[str]:
+    result = []
+    for sentence in array:
+        if sentence.endswith('?'):
+            result.append(sentence)
+        elif sentence.endswith('!'):
+            result.append(sentence)
+    return result
 
-test = ['asdad asdadsa asdada asdada']
-print(filterWordsCount(test))
+def getContainsWords(array:list[str],words:list[str]=['i', 'oraz', 'ale', 'że' ,'lub'],mincontains=2)->list[str]:
+    result = []
+    for sentence in array:
+        a = dict()
+        for word in words:
+            a.update({word: mincontains})
+        splited = Reduce.splitFromChars(sentence,ignore=Reduce.nonWhiteValues())
+        for word in splited:
+            temp = a.get(word)
+            if temp is not None:
+                if temp>0:
+                    a.update({word: temp-1})
+        isValid = True
+        for value in a.values():
+            if value>0:
+                isValid = False
+                break
+        if isValid:
+            result.append(sentence)
+    return result
+
+print(getContainsWords([" i i i i i dupa","asdasdad"]))
