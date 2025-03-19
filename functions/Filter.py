293a79ad8409abc07b7943fd305d6e5912af10a4
Slawomir <slawek.salamon44@gmail.com>
2025-03-19 19:16:49 +0100

    All funcs done

--- functions/Filter.py
+++ functions/Filter.py
@@ -1,3 +1,5 @@
+from markdown_it.common.utils import isValidEntityCode
+
 import Reduce
 
 def getZdaniaPodrzedne(array,mincount=4):
@@ -18,31 +20,51 @@ def getAskYellSentence(array:list[str])->list[str]:
     return result
 
 def getContainsWords(array:list[str],words:list[str]=['i', 'oraz', 'ale', 'że' ,'lub'],mincontains=2)->list[str]:
+    """j. Funkcja wypisująca tylko zdania, które zawierają co najmniej dwa wyrazy z następujących:
+„i”, „oraz”, „ale”, „że”, „lub”."""
+    result = []
+    for sentece in array:
+        split = Reduce.splitFromChars(sentece,ignore=Reduce.nonWhiteValues())
+        c = mincontains
+        for word in split:
+            if words.__contains__(word):
+                c-=1
+            if c<=0:
+                result.append(sentece)
+                break
+    return result
+
+
+
+
+
+"""k. Funkcja, która wypisuje na wyjściu tylko zdania w czwartym kwartylu pod względem
+długości zdania (kryterium – liczba znaków).
+l. Funkcja wypisująca tylko te zdania, których wyrazy są w porządku leksykograficznym."""
+
+
+
+def getKwartyl(array:list[str])->list[str]:
+    max_len = len(max(array,key=lambda x: len(x)))
     result = []
     for sentence in array:
-        a = dict()
-        for word in words:
-            a.update({word: mincontains})
-        splited = Reduce.splitFromChars(sentence,ignore=Reduce.nonWhiteValues())
-        for word in splited:
-            temp = a.get(word)
-            if temp is not None:
-                if temp>0:
-                    a.update({word: temp-1})
+        print(max_len,len(sentence))
+        if len(sentence)<=max_len//4:
+            result.append(sentence)
+    return result
+
+def getIfSorted(array:list[str])->list[str]:
+    result = []
+    for sentence in array:
         isValid = True
-        for value in a.values():
-            if value>0:
+        split = Reduce.splitFromChars(sentence,ignore=Reduce.nonWhiteValues())
+        for i in range(1, len(split)):
+            if split[i] < split[i - 1]:
                 isValid = False
                 break
         if isValid:
             result.append(sentence)
     return result
 
-def dupa():
 
-
-
-
-
-
-print(getContainsWords([" i i i i i dupa","asdasdad"]))
+print(getIfSorted(["a"]))
